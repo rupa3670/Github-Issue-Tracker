b6d8629d466450f97ce4c601f60b65b88db4f7a1
Tanjila Rupa <srrupaliakter@gmail.com>
2026-03-22 20:03:53 +0600

    added modal

--- script/index.js
+++ script/index.js
@@ -11,6 +11,89 @@ const loadIssue = () => {
         )
 };
 
+// issue-modal
+
+const openModal = async (id)=>{
+     const res= await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+     const data=await res.json();
+     const issue=data.data;
+     //title
+     document.getElementById("modal-title").innerText=issue.title;
+
+     //label
+issues.forEach(issue => {
+        let borderColor = "";
+        if (issue.status === 'open') {
+            borderColor = 'border-[#00A96E]';
+        }
+
+        else {
+            borderColor = 'border-[#A855F7]';
+        }
+        })
+
+      //author
+     document.getElementById("modal-author").innerText=issue.author;
+      //date
+     document.getElementById("modal-date").innerText=new Date(issue.createdAt).toLocaleDateString();
+     //description
+     document.getElementById("modal-description").innerText=issue.description;
+     //assignee
+     document.getElementById("modal-assignee").innerText= issue.assignee || issue.author;
+     //priority
+     const priorityColor=document.getElementById("modal-priority");
+     priorityColor.innerText=issue.priority.toUpperCase();
+
+    
+     //priority color change
+     if (issue.priority == "high") {
+            priorityColor.className = "bg-[#EF4444] text-white rounded-full inline-block py-1 px-3"
+            
+        }
+        else if (issue.priority == "medium") {
+            priorityColor.className = "bg-[#F59E0B]  text-white rounded-full inline-block py-1 px-3"
+            
+        }
+        else {
+            priorityColor.className = "bg-[#64748B]  text-white rounded-full inline-block py-1 px-3"
+            
+        }
+
+        const labelContainer=document.getElementById("modal-labels")
+        labelContainer.innerHTML="";
+        issue.labels.forEach(label=>{
+            let labelBg="bg-[#DEFCE8]"
+            let labelText="text-[#00A96E]"
+            let labelIcon=`<i class="fa-solid fa-tag"></i>`;
+
+            if(label.toLowerCase()==="bug"){
+                labelBg="bg-[#FECACA]";
+                labelText="text-[#EF4444]"
+                labelIcon=
+                `<i class="fa-solid fa-bug"
+                > </i>`
+            }
+           else if(label.toLowerCase()==="help wanted"){
+                labelBg="bg-[#FFF8DB]";
+                labelText="text-[#D97706]"
+                labelIcon=
+                `<i class="fa-regular fa-life-ring"></i>`
+            }
+            else{
+                 labelBg="bg-[#DEFCE8]"
+             labelText="text-[#00A96E]"
+             labelIcon=`<img src="./images/Vector.png" alt="">`;
+            }
+            const div=document.createElement("div");
+            div.className=`flex items-center px-3 py-1 ${labelBg} ${labelText} gap-1 rounded-full text-xs font semibold`
+            div.innerHTML=`${labelIcon}<span>${label}</span>`
+
+            labelContainer.appendChild(div);
+        });
+        document.getElementById("issue_modal").showModal();
+
+}
+
 
 //button toggling
 const btnAll = document.getElementById("btn-all")
@@ -85,8 +168,15 @@ const displayIssues = (issues) => {
         const dateRaw = issue.createdAt;
         const displayDate = new Date(dateRaw).toLocaleDateString();
         const card = document.createElement("div")
+        card.onclick=()=>openModal(issue.id);
+    card.className="cursor-pointer h-full";
+    
         card.innerHTML = `
      <div class="bg-white p-4 rounded-[8px] border-t-4 ${borderColor} shadow-sm h-full flex flex-col justify-between  ">
+
+        
+
+
                 <div class="flex justify-between">
                <div  class="${issue.status === 'open' ? 'bg-[#CBFADB] text-green-700' :
                 'bg-[#F0E2FF] text-[#A855F7] rounded-full p-[2px]'} rounded-full">
@@ -127,7 +217,7 @@ const displayIssues = (issues) => {
                 }).join('')}</div>
                
             <hr class="border-gray-300 mb-[15px]">
-             <div class="text-[#64748B]">
+             <div class="text-[#64748B] text-left">
             <p >#${issue.id} by <span>${issue.author}</span></p>
             <p>${displayDate}</p>
         </div>
@@ -137,6 +227,7 @@ const displayIssues = (issues) => {
         //4. append into container
         issueContainer.append(card);
     })
+    
 
 };
 loadIssue();
